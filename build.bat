8c7e492867ccc60d73f71cc8e7a3f1ea02316681
Ilya Akkuzin <gr3yknigh1@gmail.com>
2025-03-28 19:51:19 +0300

    Replace pyinvoke with htask in build.bat

--- build.bat
+++ build.bat
@@ -1,19 +1,22 @@
-::
-:: FILE          build.bat
-::
-:: AUTHORS
-::               Ilya Akkuzin <gr3yknigh1@gmail.com>
-::
-:: NOTICE        (c) Copyright 2025 by Ilya Akkuzin. All rights reserved.
-::
+@rem 
+@rem  FILE          build.bat
+@rem 
+@rem  AUTHORS
+@rem                Ilya Akkuzin <gr3yknigh1@gmail.com>
+@rem 
+@rem  NOTICE        (c) Copyright 2025 by Ilya Akkuzin. All rights reserved.
+@rem 
 
 @echo off
+
+set project_folder=%~dp0
 
 set build_type=%1
 shift
 
 if [%build_type%]==[] set build_type=Debug
 
-inv build --build-type %build_type%
+htask -C %project_folder% build --build-type %build_type%
 
-:: set /p DUMMY=Hit ENTER to continue...
+pause
+
